8cad7175d7e9ffe77970fa01207deaab6dadc46a
Keyur Patel <ekeyur@gmail.com>
2017-10-11 11:14:43 -0400

    wiring up the graphs for weather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,18 @@
+import axios from 'axios';
+
 const API_KEY = '45b8f40cbeefd59cd13eac204dd16e32';
-const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+// const API_KEY = '6a78596d062df78380eff5944c4e5567';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
+
+
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
-
-    
+    const request = axios.get(url);
     return {
         type: FETCH_WEATHER,
-
+        payload: request
     };
 }
